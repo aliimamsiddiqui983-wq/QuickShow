d72ea3ae37db53d6cbfa83974684cc866d64d3a5
aliimamsiddiqui983-wq <aliimamsiddiqui983@gmail.com>
2026-09-18 15:02:47 +0530

    Update server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-
-import clerkWebhookRouter from './routes/clerkWebhook.js';
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
@@ -7,14 +5,20 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import clerkWebhookRouter from './routes/clerkWebhook.js';
 
 
 const app = express();
 const port = 3000;
 await connectDB();
 
-app.use(express.json());
 app.use(cors())
+
+// ⚠️ Webhook route SABSE PEHLE, express.json() se pehle:
+app.use('/api/webhooks/clerk', clerkWebhookRouter);
+
+// Ab baaki sab routes ke liye JSON parsing:
+app.use(express.json());
 app.use(clerkMiddleware())
 
 // API Routes
@@ -22,7 +26,6 @@ app.get('/' , (req, res) => {
     res.send("Server is Live ! ")
 })
 app.use('/api/inngest', serve({client: inngest, functions}))
-app.use('/api/webhooks/clerk', clerkWebhookRouter);
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
